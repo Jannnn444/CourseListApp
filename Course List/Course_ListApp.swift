237eb779c7d87316487fb2c7436a1684c62e81ae
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-03-13 17:34:52 +0800

    Initial Commit

---
+++ Course List/Course_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ListApp.swift
+//  Course List
+//
+//  Created by yucian huang on 2024/3/13.
+//
+
+import SwiftUI
+
+@main
+struct Course_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
